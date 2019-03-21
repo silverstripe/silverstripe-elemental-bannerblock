silverstripe/silverstripe-elemental-bannerblock

commit 3cd243854b7b4d68e86dda8993bfdce93207abdb

API BlockLinkField is now a React driven component
Data value is still injected as JSON, the React component parses it and renders itself
as well as the actions menu and insert link modal itself. This removes the coupling to
TinyMCE.

src/Form/BlockLinkField.php
@@ -12,8 +12,7 @@ class BlockLinkField extends FormField
 {
     protected $schemaDataType = FormField::SCHEMA_DATA_TYPE_CUSTOM;
 
-    protected $schemaComponent = 'BlockLinkFieldComponent';
-
+    protected $schemaComponent = 'BlockLinkField';
 
     /**
      * Cache for parsed value
@@ -56,19 +55,8 @@ class BlockLinkField extends FormField
         if ($this->parsedValue) {
             return $this->parsedValue;
         }
-
-        $parsedValue = Convert::json2array($this->dataValue());
+        $parsedValue = json_decode($this->dataValue(), true);
         return $this->parsedValue = ArrayData::create((array) $parsedValue);
-    }
-
-    /**
-     * Get whether a {@link SiteTree} link has been defined on this link field
-     *
-     * @return bool
-     */
-    public function getLinkDefined()
-    {
-        return (bool) $this->getParsedValue()->PageID;
     }
 
     /**
@@ -82,7 +70,6 @@ class BlockLinkField extends FormField
         if (!$pageId) {
             return null;
         }
-
         return DataObject::get_by_id(SiteTree::class, $pageId);
     }
 
@@ -94,38 +81,7 @@ class BlockLinkField extends FormField
     public function getLinkRelativeUrl()
     {
         $page = $this->getLinkPage();
-
         return $page ? '/' . ltrim($page->URLSegment, '/') : '';
-    }
-
-    /**
-     * Get the link text/title
-     *
-     * @return string
-     */
-    public function getLinkText()
-    {
-        return trim($this->getParsedValue()->Text);
-    }
-
-    /**
-     * Get the link "description", used for titles or alt text
-     *
-     * @return string
-     */
-    public function getLinkDescription()
-    {
-        return $this->getParsedValue()->Description;
-    }
-
-    /**
-     * Get whether to open the link in a new window
-     *
-     * @return bool
-     */
-    public function getLinkTargetBlank()
-    {
-        return (bool) $this->getParsedValue()->TargetBlank;
     }
 
     /**
@@ -137,9 +93,6 @@ class BlockLinkField extends FormField
     public function setShowLinkText($showLinkText)
     {
         $this->showLinkText = (bool) $showLinkText;
-
-        $this->setAttribute('data-showlinktext', (int) $this->showLinkText);
-
         return $this;
     }
 
@@ -151,5 +104,37 @@ class BlockLinkField extends FormField
     public function getShowLinkText()
     {
         return $this->showLinkText;
+    }
+
+    /**
+     * When not used in a React form factory context, this adds the schema and state data to SilverStripe
+     * template rendered attributes lists
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        $attributes = parent::getAttributes();
+
+        $attributes['data-schema'] = json_encode($this->getSchemaData());
+        $attributes['data-state'] = json_encode($this->getSchemaState());
+
+        return $attributes;
+    }
+
+    /**
+     * Add some extra props for the React component to work with
+     *
+     * {@inheritDoc}
+     */
+    public function getSchemaDataDefaults()
+    {
+        $schema = parent::getSchemaDataDefaults();
+        $schema['data'] = array_merge($schema['data'], [
+            'showLinkText' => $this->getShowLinkText(),
+            'linkedPage' => $this->getLinkPage() ? $this->getLinkPage()->toMap() : [],
+            'title' => $this->Title(),
+        ]);
+        return $schema;
     }
 }

.sass-lint.yml
@@ -1,0 +1,178 @@
+# sass-lint config to match the AirBNB style guide
+files:
+  include: '**/client/src/**/*.scss'
+  ignore:
+    - 'client/src/styles/legacy/*'
+    - 'admin/client/src/styles/legacy/*'
+options:
+  formatter: stylish
+  merge-default-rules: false
+rules:
+  # Warnings
+  # Things that require actual refactoring are marked as warnings
+  class-name-format:
+    - 1
+    - convention: hyphenatedbem
+  placeholder-name-format:
+    - 1
+    - convention: hyphenatedlowercase
+  nesting-depth:
+    - 1
+    - max-depth: 3
+  no-ids: 1
+  no-important: 1
+  no-misspelled-properties:
+    - 1
+    - extra-properties:
+       - "-moz-border-radius-topleft"
+       - "-moz-border-radius-topright"
+       - "-moz-border-radius-bottomleft"
+       - "-moz-border-radius-bottomright"
+  variable-name-format:
+    - 1
+    - allow-leading-underscore: true
+      convention: hyphenatedlowercase
+  no-extends: 1
+
+  # Warnings: these things are preferential rather than mandatory
+  no-css-comments: 1
+
+  # Errors
+  # Things that can be easily fixed are marked as errors
+  indentation:
+    - 2
+    - size: 2
+  final-newline:
+    - 2
+    - include: true
+  no-trailing-whitespace: 2
+  border-zero:
+    - 2
+    - convention: '0'
+  brace-style:
+    - 2
+    - allow-single-line: true
+  clean-import-paths:
+    - 2
+    - filename-extension: false
+      leading-underscore: false
+  no-debug: 2
+  no-empty-rulesets: 2
+  no-invalid-hex: 2
+  no-mergeable-selectors: 2
+  # no-qualifying-elements:
+  #   - 1
+  #   - allow-element-with-attribute: false
+  #     allow-element-with-class: false
+  #     allow-element-with-id: false
+  no-trailing-zero: 2
+  no-url-protocols: 2
+  quotes:
+    - 2
+    - style: double
+  space-after-bang:
+    - 2
+    - include: false
+  space-after-colon:
+    - 2
+    - include: true
+  space-after-comma:
+    - 2
+    - include: true
+  space-before-bang:
+    - 2
+    - include: true
+  space-before-brace:
+    - 2
+    - include: true
+  space-before-colon: 2
+  space-between-parens:
+    - 2
+    - include: false
+  trailing-semicolon: 2
+  url-quotes: 2
+  zero-unit: 2
+  single-line-per-selector: 2
+  one-declaration-per-line: 2
+  empty-line-between-blocks:
+    - 2
+    - ignore-single-line-rulesets: true
+
+
+  # Missing rules
+  # There are no sass-lint rules for the following AirBNB style items, but thess
+  # - Put comments on their own line
+  # - Put property delcarations before mixins
+
+  # Disabled rules
+
+  # These are other rules that we may wish to consider using in the future
+  # They are not part of the AirBNB CSS standard but they would introduce some strictness
+  # bem-depth: 0
+  # variable-for-property: 0
+  # no-transition-all: 0
+  # hex-length:
+  #   - 1
+  #   - style: short
+  # hex-notation:
+  #   - 1
+  #   - style: lowercase
+  # property-units:
+  #   - 1
+  #   - global:
+  #       - ch
+  #       - em
+  #       - ex
+  #       - rem
+  #       - cm
+  #       - in
+  #       - mm
+  #       - pc
+  #       - pt
+  #       - px
+  #       - q
+  #       - vh
+  #       - vw
+  #       - vmin
+  #       - vmax
+  #       - deg
+  #       - grad
+  #       - rad
+  #       - turn
+  #       - ms
+  #       - s
+  #       - Hz
+  #       - kHz
+  #       - dpi
+  #       - dpcm
+  #       - dppx
+  #       - '%'
+  #     per-property: {}
+  # force-attribute-nesting: 1
+  # force-element-nesting: 1
+  # force-pseudo-nesting: 1
+  # function-name-format:
+  #   - 1
+  #   - allow-leading-underscore: true
+  #     convention: hyphenatedlowercase
+  # no-color-literals: 1
+  # no-duplicate-properties: 1
+  # mixin-name-format:
+  #   - 1
+  #   - allow-leading-underscore: true
+  #     convention: hyphenatedlowercase
+  # shorthand-values:
+  #   - 1
+  #   - allowed-shorthands:
+  #       - 1
+  #       - 2
+  #       - 3
+  # leading-zero:
+  #   - 1
+  #   - include: false
+  # no-vendor-prefixes:
+  #   - 1
+  #   - additional-identifiers: []
+  #     excluded-identifiers: []
+  #     placeholder-in-extend: 1
+  # no-color-keywords: 2

_config/config.yml
@@ -9,6 +9,6 @@ DNADesign\Elemental\Controllers\ElementController:
   default_styles:
     - silverstripe/elemental-bannerblock:client/dist/styles/frontend-default.css
 
-SilverStripe\Admin\LeftAndMain:
-  extra_requirements_javascript:
-    - 'silverstripe/elemental-bannerblock:client/dist/js/bundle.js'
+#SilverStripe\Admin\LeftAndMain:
+#  extra_requirements_javascript:
+#    - 'silverstripe/elemental-bannerblock:client/dist/js/bundle.js'

client/dist/styles/bundle.css
@@ -1,2 +1,1 @@
 .form__field-holder .block-link-field{box-shadow:none;-webkit-box-shadow:none}.form__field-holder .block-link-field:hover{background-color:#f0f4f7;cursor:pointer}.block-link-field__icon:before{content:"2";font-family:silverstripe;font-size:1.4772rem;left:6px;margin-right:18px;position:relative}.block-link-field__content--message{font-style:italic}.block-link-field__link{color:#0071c4}.block-link-field__title{margin-right:1.2308rem}@media (min-width:544px){.block-link-field__title{margin-right:6.154rem}}
-/*# sourceMappingURL=bundle.css.map*/

client/dist/styles/frontend-default.css
@@ -1,2 +1,1 @@
-.silverstripe__elementalbannerblock__block__bannerblock{position:relative;width:100%;display:-webkit-box;display:-webkit-flex;display:flex;height:200px;-webkit-box-align:center;-webkit-align-items:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;justify-content:center}.banner-element__image{background-color:#eee;display:-webkit-box;display:-webkit-flex;display:flex;-webkit-box-flex:1;-webkit-flex-grow:1;flex-grow:1;height:200px}.banner-element__image img{border:none;-o-object-fit:cover;object-fit:cover;width:100%}.banner-element__content{left:40px;position:absolute;width:90%}.banner-element__call-to-action{padding:10px;background:red;color:#fff!important;display:inline-block;text-decoration:none;margin-bottom:10px}.banner-element__call-to-action:hover{border:none!important}
-/*# sourceMappingURL=frontend-default.css.map*/
+.silverstripe__elementalbannerblock__block__bannerblock{position:relative;width:100%;display:-webkit-box;display:-webkit-flex;display:flex;height:200px;-webkit-box-align:center;-webkit-align-items:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;justify-content:center}.banner-element__image{background-color:#eee;display:-webkit-box;display:-webkit-flex;display:flex;-webkit-box-flex:1;-webkit-flex-grow:1;flex-grow:1;height:200px}.banner-element__image img{border:0;-o-object-fit:cover;object-fit:cover;width:100%}.banner-element__content{left:40px;position:absolute;width:90%}.banner-element__call-to-action{padding:10px;background:red;color:#fff!important;display:inline-block;text-decoration:none;margin-bottom:10px}.banner-element__call-to-action:hover{border:0!important}

client/src/boot/registerComponents.js
@@ -1,10 +1,10 @@
 import Injector from 'lib/Injector';
-import BlockLinkFieldComponent from 'components/BlockLinkField/BlockLinkFieldComponent';
+import BlockLinkField from 'components/BlockLinkField/BlockLinkField';
 import BlockLinkFieldActions from 'components/BlockLinkFieldActions/BlockLinkFieldActions';
 
 export default () => {
   Injector.component.registerMany({
-    BlockLinkFieldComponent,
-    BlockLinkFieldActions
+    BlockLinkField,
+    BlockLinkFieldActions,
   });
 };

client/src/bundles/bundle.js
@@ -1,2 +1,2 @@
+require('../legacy/BlockLinkFieldEntwine');
 require('boot');
-

commit 4cdfa4b524f992e743f87ef15e2a3f48a3f3fbe3

FIX Restore readonly version (fixes #21)
Regression from https://github.com/silverstripe/silverstripe-elemental-bannerblock/commit/3cd243854b7b4d68e86dda8993bfdce93207abdb#diff-9febe33e35142c914c40f462aa800eb6

src/Form/BlockLinkField.php
@@ -74,6 +74,34 @@ class BlockLinkField extends FormField
     }
 
     /**
+     * Get the link text/title
+     *
+     * @return string
+     */
+    public function getLinkText()
+    {
+        return trim($this->getParsedValue()->Text);
+    }
+    /**
+     * Get the link "description", used for titles or alt text
+     *
+     * @return string
+     */
+    public function getLinkDescription()
+    {
+        return $this->getParsedValue()->Description;
+    }
+    /**
+     * Get whether to open the link in a new window
+     *
+     * @return bool
+     */
+    public function getLinkTargetBlank()
+    {
+        return (bool) $this->getParsedValue()->TargetBlank;
+    }
+
+    /**
      * Get the relative URL for the linked {@link SiteTree} object, with a leading slash
      *
      * @return string